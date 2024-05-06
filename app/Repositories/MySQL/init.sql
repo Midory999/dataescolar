f14c12beb6fc1e150d59692622f9e3f56c29f97f
fadrian06 <franyeradriansanchez@gmail.com>
2024-05-05 21:03:34 -0400

    Actualizado el script mysql

--- app/Repositories/MySQL/init.sql
+++ app/Repositories/MySQL/init.sql
@@ -1,65 +1,65 @@
-CREATE DATABASE IF NOT EXISTS dataescolar;
+DROP DATABASE IF EXISTS dataescolar;
+CREATE DATABASE dataescolar;
 USE dataescolar;
 
-CREATE TABLE IF NOT EXISTS usuarios(
+CREATE TABLE usuarios (
 	id VARCHAR(255) PRIMARY KEY,
 	nombre TEXT NOT NULL,
 	apellido TEXT NOT NULL,
-	cedula INT UNIQUE NOT NULL,
+	cedula INTEGER UNIQUE NOT NULL,
 	clave TEXT NOT NULL,
-	rol ENUM('Administrador', 'Director', 'Secretario', 'Profesor') NOT NULL,
+	rol TEXT NOT NULL CHECK (rol IN ('Administrador', 'Director', 'Secretario', 'Profesor')),
 	pregunta TEXT NOT NULL,
 	respuesta TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Representantes(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Representantes(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	nombres VARCHAR(30) NOT NULL,
 	apellidos VARCHAR(30) NOT NULL,
 	cedula INT UNIQUE NOT NULL,
 	genero TEXT NOT NULL,
 	direccion TEXT NOT NULL,
 	correo VARCHAR(50) NOT NULL UNIQUE,
-	telefono VARCHAR(15)NOT NULL UNIQUE,
+	telefono VARCHAR(15) NOT NULL UNIQUE,
 	estudio_socioeconomico TEXT NOT NULL,
 	tipo_sangre VARCHAR(2) NOT NULL,
 	ocupacion TEXT NOT NULL,
 	lugar_trabajo TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Niveles(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Niveles(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	codigo VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Areas(
-	codigo INT PRIMARY KEY AUTO_INCREMENT,
-	nombre text(20) NOT NULL
+CREATE TABLE Areas (
+	codigo INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS Periodos(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	inicio INT UNIQUE NOT NULL,
-	fin INT UNIQUE NOT NULL
+CREATE TABLE Periodos (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	inicio INTEGER UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Lapsos(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Lapsos (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	nombre TEXT NOT NULL,
-	inicio DATE NOT NULL,
-	fin DATE NOT NULL,
-	id_Periodo INT NOT NULL,
-	FOREIGN KEY (id_Periodo) REFERENCES Periodos(id)
+	inicio DATE NOT NULL UNIQUE,
+	fin DATE NOT NULL UNIQUE,
+	id_periodo INTEGER NOT NULL,
+	FOREIGN KEY (id_periodo) REFERENCES Periodos(id)
 );
 
-CREATE TABLE IF NOT EXISTS Estudiantes(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Estudiantes(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	nombres VARCHAR(20) NOT NULL,
 	apellidos VARCHAR(30) NOT NULL,
 	cedula INT UNIQUE NOT NULL,
 	fecha_nacimiento DATE NOT NULL,
 	lugar_nacimiento DATE NOT NULL,
-	edad INT(2) NOT NULL,
+	edad INTEGER NOT NULL,
 	genero VARCHAR(1) NOT NULL,
 	tipo_parto TEXT NOT NULL,
 	compromisos TEXT NOT NULL,
@@ -72,63 +72,67 @@ CREATE TABLE IF NOT EXISTS Estudiantes(
 	ingreso DATE NOT NULL,
 	estatus VARCHAR(10) NOT NULL,
 	descripcion TEXT NOT NULL,
-	id_Representante INT NOT NULL,
+	id_Representante INTEGER NOT NULL,
 	FOREIGN KEY (id_Representante) REFERENCES Representantes(id)
 );
 
-CREATE TABLE IF NOT EXISTS Profesores(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Profesores(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	nombres VARCHAR(25) NOT NULL,
 	apellidos VARCHAR(30) NOT NULL,
 	cedula INT UNIQUE NOT NULL,
 	estatus VARCHAR(10) NOT NULL,
-	especialidad text(100) NOT NULL,
-	direccion text(80) NOT NULL,
+	especialidad TEXT NOT NULL,
+	direccion VARCHAR(255) NOT NULL UNIQUE,
 	correo VARCHAR(50) NOT NULL UNIQUE,
 	telefono VARCHAR(15) NOT NULL UNIQUE,
 	fecha_ingreso date NOT NULL,
 	fecha_nacimiento date NOT NULL,
 	edad VARCHAR(2) NOT NULL,
-	genero TEXT(1) NOT NULL,
-	vacunas TEXT(100) NOT NULL,
+	genero TEXT NOT NULL,
+	vacunas TEXT NOT NULL,
 	carga_horaria VARCHAR(20) NOT NULL,
 	codigo_idependencia VARCHAR(20) NOT NULL,
-	codigo_Area INT NOT NULL,
+	codigo_Area INTEGER NOT NULL,
 	FOREIGN KEY (codigo_Area) REFERENCES Areas(codigo)
 );
 
-CREATE TABLE IF NOT EXISTS Aulas(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Aulas (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	nombre TEXT NOT NULL,
-	id_Profesor INT NOT NULL,
-	id_Estudiante INT NOT NULL,
+	id_Profesor INTEGER NOT NULL,
+	id_Estudiante INTEGER NOT NULL,
 	FOREIGN KEY (id_Profesor) REFERENCES Profesores(id),
 	FOREIGN KEY (id_Estudiante) REFERENCES Estudiantes(id)
 );
 
-CREATE TABLE IF NOT EXISTS Informes(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Informes (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	diagnostico TEXT NOT NULL,
 	lapso1 TEXT NOT NULL,
 	lapso2 TEXT NOT NULL,
 	lapso3 TEXT NOT NULL,
 	informes_final TEXT NOT NULL,
-	id_Estudiante INT NOT NULL,
-	id_Profesor INT NOT NULL,
-	codigo_Area INT NOT NULL,
-	id_Nivel INT NOT NULL,
+	id_Estudiante INTEGER NOT NULL,
+	id_Profesor INTEGER NOT NULL,
+	codigo_Area INTEGER NOT NULL,
+	id_Nivel INTEGER NOT NULL,
 	FOREIGN KEY (id_Estudiante) REFERENCES Estudiantes(id),
 	FOREIGN KEY (id_Profesor) REFERENCES Profesores(id),
 	FOREIGN KEY (codigo_Area) REFERENCES Areas(codigo),
 	FOREIGN KEY (id_Nivel) REFERENCES Niveles(id)
 );
 
-CREATE TABLE IF NOT EXISTS Inscripciones(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	id_Estudiante INT NOT NULL,
-	id_Periodo INT NOT NULL,
-	id_Nivel INT NOT NULL,
-	FOREIGN KEY (id_Estudiante) REFERENCES Estudiantes(id),
-	FOREIGN KEY (id_Periodo) REFERENCES Periodos(id),
-	FOREIGN KEY (id_Nivel) REFERENCES Niveles(id)
+CREATE TABLE Inscripciones (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	id_estudiante INTEGER NOT NULL,
+	id_periodo INTEGER NOT NULL,
+	id_nivel INTEGER NOT NULL,
+	FOREIGN KEY (id_estudiante) REFERENCES Estudiantes(id),
+	FOREIGN KEY (id_periodo) REFERENCES Periodos(id),
+	FOREIGN KEY (id_nivel) REFERENCES Niveles(id)
 );
+
+-- REGISTROS
+INSERT INTO Areas(codigo, nombre) VALUES (1, 'Lenguaje'), (2, 'Cognitiva'),
+(3, 'Motora'), (4, 'Moral y Espiritual'), (6, 'Comunicación'), (5, 'Afectiva');
