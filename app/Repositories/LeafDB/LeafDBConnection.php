13b7e233782d02ebb5faa95f4a4bf06d762781a3
fadrian06 <franyeradriansanchez@gmail.com>
2024-05-05 21:11:04 -0400

    Ajustes en la conexión MySQL

--- app/Repositories/LeafDB/LeafDBConnection.php
+++ app/Repositories/LeafDB/LeafDBConnection.php
@@ -4,40 +4,39 @@ namespace App\Repositories\LeafDB;
 
 use App\Core\Env;
 use Leaf\Db;
+use RuntimeException;
 
 abstract class LeafDBConnection {
 	protected static ?Db $db = null;
 
 	function __construct() {
 		if (self::$db === null) {
-			self::$db = new Db([
-				'dbtype' => Env::get('DB_CONNECTION'),
-				'dbname' => __DIR__ . '/../SQLite/' . Env::get('DB_DATABASE')
-			]);
-
-			// self::prepareDB();
+			self::$db = new Db(self::getDbParams());
 		}
 	}
 
-	// protected static function db(): Db {
 	public static function db(): Db {
 		return self::$db;
 	}
 
-	private static function prepareDB(): void {
-		assert(self::$db !== null);
-
-		$sqlFile = match (strtolower((string) Env::get('DB_CONNECTION'))) {
-			'sqlite' => __DIR__ . '/../SQLite/init.sql',
-			'mysql' => __DIR__ . '/../MySQL/init.sql',
-			default => throw new \Error('Base de datos no soportada')
-		};
-
-		$queries = explode(';', (string) file_get_contents($sqlFile));
-		foreach ($queries as $query) {
-			if ($query) {
-				self::$db->query($query)->execute();
-			}
+	private static function getDbParams(): array {
+		if (strtolower(Env::get('DB_CONNECTION')) === 'sqlite') {
+			return [
+				'dbtype' => Env::get('DB_CONNECTION'),
+				'dbname' => __DIR__ . '/../SQLite/' . Env::get('DB_DATABASE')
+			];
 		}
+
+		if (strtolower(Env::get('DB_CONNECTION')) === 'mysql') {
+			return [
+				'dbtype' => Env::get('DB_CONNECTION'),
+				'dbname' => Env::get('DB_DATABASE'),
+				'host' => Env::get('DB_HOST'),
+				'user' => Env::get('DB_USERNAME'),
+				'password' => Env::get('DB_PASSWORD')
+			];
+		}
+
+		throw new RuntimeException('DBtype ' . Env::get('DB_CONNECTION') . ' not supported');
 	}
 }
