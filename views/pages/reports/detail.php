9d9f82ec147b82502a058e60249df34e755d8374
fadrian06 <franyeradriansanchez@gmail.com>
2024-06-24 21:48:37 -0400

    Avances en el maquetado del informe

--- views/pages/reports/detail.php
+++ views/pages/reports/detail.php
@@ -65,7 +65,7 @@ $meses = [
 			</b>
 		</h2>
 	</header>
-	<main>
+	<main class="w3-margin-bottom">
 		<h1 class="w3-large w3-center">
 			<b>INFORME DESCRIPTIVO DEL RENDIMIENTO ESTUDIANTIL</b>
 		</h1>
@@ -82,14 +82,49 @@ $meses = [
 			Representante: <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 15) . $report->student->representative . str_repeat('&nbsp;', 15) ?></u>
 			Docente: <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . $report->teacher . str_repeat('&nbsp;', 10) ?></u>
 		</section>
-		<section class="w3-justify w3-padding-large">
+		<section class="w3-large w3-justify w3-padding-large">
 			<h2>
 				<b>INFORME FINAL</b>
 			</h2>
 			<u class="dancing w3-xlarge"><?= $report->finalInform ?></u>
 		</section>
+		<section class="w3-large w3-justify w3-padding-large">
+			<h3>
+				<b>JUICIOS DESCRIPTIVOS:</b>
+			</h3>
+			<h4>
+				Observaciones y recomendaciones:
+			</h4>
+			<u class="dancing w3-xlarge"><?= $report->diagnostic ?></u>
+		</section>
+		<section class="w3-large w3-justify w3-padding-large">
+			<h3>
+				<b>CERTIFICADO DE PROMOCIÓN</b>
+			</h3>
+			Quien subscribe: <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 16) . str_repeat('&nbsp;', 16) ?></u>
+			Titular de la C.I N° <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 16) . str_repeat('&nbsp;', 16) ?></u>
+			<br />
+			Directora del plantel certificó que el alumno cursó en esta institución el <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+			Grado y de acuerdo al rendimiento estudiantil se encuentra
+			<br />
+			<strong>Promovido: </strong>
+			<b>SI</b> <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+			<b>NO</b> <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+			<br />
+			Al <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+			de educación <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+			según literal <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+		</section>
 	</main>
-	<footer></footer>
+	<footer class="w3-margin-top w3-padding-large w3-large w3-justify">
+		Sello: <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 30) . str_repeat('&nbsp;', 30) ?></u>
+		<br />
+		<div class="w3-margin-top">
+			Directora <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+			Docente <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+			Representante <u class="dancing w3-xlarge"><?= str_repeat('&nbsp;', 10) . str_repeat('&nbsp;', 10) ?></u>
+		</div>
+	</footer>
 </body>
 
 </html>
